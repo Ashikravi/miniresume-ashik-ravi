abf7c492d943047faa929ac5adaf619c83533af6
Ashikravi <ashikravi200@gmail.com>
2026-03-07 01:19:59 +0530

    Fix:candidate model and gitignore

--- models.py
+++ models.py
@@ -5,14 +5,25 @@ class Candidate(Base):
     __tablename__ = "candidates"
 
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String)
+
+    full_name = Column(String, nullable=False)
+
     dob = Column(String)
-    contact_email = Column(String, unique=True)
+
+    contact_email = Column(String, unique=True, index=True)
+
     contact_number = Column(String)
+
     contact_address = Column(String)
+
     education = Column(String)
+
     graduation_year = Column(Integer)
+
     experience = Column(Integer)
+
     skills = Column(String)
+
     resume_file = Column(String)
+
     created_at = Column(String)
